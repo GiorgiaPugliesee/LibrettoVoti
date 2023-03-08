62d582cfd33237dc4cb2558ab3d6cf154443da7a
giorg <giorg@172.21.118.89>
2023-03-08 13:36:45 +0100

    Prima esercitazione LibrettoVoti

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,5 +1,60 @@
 package it.polito.tdp.libretto.model;
 
-public class Libretto {
+import java.util.*;
 
+public class Libretto {
+	
+	private List<Voto> listaVoti;
+	
+	public Libretto() {
+		listaVoti = new LinkedList<Voto>();
+	}
+	
+	/**
+	 * Aggiungi un nuovo voto al libretto
+	 * (per ora non fa nessun controllo)
+	 * @param v  il Voto da aggiungere
+	 * @return true
+	 */
+	
+	public boolean add(Voto v) {
+		return listaVoti.add(v);
+	}
+	
+	public void stampa() {
+		for (Voto v : listaVoti) {
+			System.out.println(v);
+		}
+	}
+
+	public void stampaVotoUguale(int valore) {
+		for (Voto v : listaVoti) {
+			if (v.getVotoOttenuto() == valore) {
+				System.out.println(v);
+			}
+		}
+	}
+	
+	public Voto cercaVotoPerCorso(String corso) {
+		for (Voto v : listaVoti) {
+			if (v.getNomeCorso().equals(corso)) {
+			//if (v.getNomeCorso().compareTo(corso)==0) {
+				return v;
+			}
+		}
+		return null;
+		
+		// throw new RuntimeException("Voto non trovato");  preferibile per casi non prevedibili/comuni
+	}
+	
+	public boolean esisteVoto(Voto vNuovo) {
+		for (Voto v : listaVoti) {
+			//if (v.equalsCorsoPunti(vNuovo))  {}  conviene creare metodi equals specifici se servono a più metodi
+			if (v.getNomeCorso().equals(vNuovo.getNomeCorso()) && v.getVotoOttenuto() == vNuovo.getVotoOttenuto()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -1,10 +1,36 @@
 package it.polito.tdp.libretto.model;
 
+import java.time.LocalDate;
+
 public class TestLibretto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+		Libretto libretto = new Libretto();
+		libretto.add(new Voto("Analisi 1", 29, LocalDate.of(2021, 2, 15)));
+		libretto.add(new Voto("Fisica 2", 21, LocalDate.of(2022, 6, 10)));
+		libretto.add(new Voto("Chimica", 25, LocalDate.of(2021, 2, 1)));
 
+		System.out.println("\nStampo le informazioni di tutti i corsi inseriti: ");
+		libretto.stampa();
+		
+		System.out.println("\nStampo le informazioni di tutti i corsi con voto uguale a 25: ");
+		libretto.stampaVotoUguale(25);
+		
+		System.out.println("\nCerco le informazioni del corso di Analisi 1: ");
+		Voto v = libretto.cercaVotoPerCorso("Analisi 1");
+		System.out.println(v);
+		
+		System.out.println("\nCerco le informazioni del corso di Analisi 2 (non esiste): ");
+		v = libretto.cercaVotoPerCorso("Analisi 2");
+		System.out.println(v);
+		
+		System.out.println("\nVerifico se un Voto esiste già nel libretto: ");
+		Voto v1 = new Voto("Analisi 1", 29, LocalDate.of(2025, 2, 15));
+		Voto v2 = new Voto("Analisi 1", 28, LocalDate.of(2025, 2, 15));
+		
+		System.out.println(v1 + " è duplicato + " + libretto.esisteVoto(v1));
+		System.out.println(v2 + " è duplicato + " + libretto.esisteVoto(v2));
 	}
 
 }
